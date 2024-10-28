a2f0cb6bb1962cf3e3dfb06759f2662b621f189d
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2024-10-28 11:50:37 +0530

    Update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./app.scss"
+
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+  </div>;
+};
+
+export default App;
