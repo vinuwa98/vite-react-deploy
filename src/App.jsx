dccf07481c5c4f73fa23c5c1b36cfb74b1d30fad
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2024-11-12 21:13:02 +0530

    Add Cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
+
 import "./app.scss";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
-import Services from "./components/services/Services"; // Import Services component
+import Services from "./components/services/Services";
 
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="Homepage">
         <Navbar />
         <Hero />
       </section>
-
       <section id="Services">
-        <Parallax type="Services" />
+        <Parallax type="services" />
       </section>
       <section>
-        <Services /> {/* Render Services component */}
+        <Services />
       </section>
       <section id="Portfolio">
-        <Parallax type="Portfolio" />
+        <Parallax type="portfolio" />
       </section>
-      <Portfolio/>
-      <section id="Contact">Contact</section>
-      <section id="AboutUs">About Us</section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
     </div>
   );
 };
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,7 +12,7 @@ const variants = {
     y: 0,
     opacity: 1,
     transition: {
-      duration: 0.5,
+      duration: 0.2,
       staggerChildren: 0.1,
     },
   },
@@ -31,10 +31,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_94y20xo",
-        "template_v10u2oh",
+        "service_dx8pydd",
+        "template_o740pcq",
         formRef.current,
-        "pX_2hasGmGcuvjXIW"
+        "Gt3O6vLbyV8BO-m5Y"
       )
       .then(
         (result) => {
@@ -58,15 +58,15 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>vinuwa98@outlook.com <br/> vinuwa98@gmail.com </span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Hello street New York</span>
+          <span>No: 280 <br/> Hewaheta Road <br/> Buwella,Rikillagaskada <br/>Kandy, Central Province <br/> Postal Code 20730</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
-          <span>+1 234 5678</span>
+          <span>+94 70 77 00 77 1<br/> +94 78 89 12 26 0</span>
         </motion.div>
       </motion.div>
       <div className="formContainer">
