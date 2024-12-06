a759f47cea5273ab36c9787374404fef7ad5cfcd
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2024-12-06 23:15:40 +0530

    Update Details

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -74,17 +74,16 @@ const Services = () => {
             </motion.div>
             <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
                 <h2>A/L ICT Tutor</h2>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    At voluptas asperiores illo hic expedita repellat est saepe dolorem voluptates culpa alias,
-                    debitis amet tempora vel et iusto. Nobis, necessitatibus cupiditate!
+                <p> I am passionate about education and enjoy sharing my knowledge with students. I provide Advanced Level ICT tuition and 
+                    Ordinary Level Science tuition guiding students to excel in their studies and achieve their academic goals.
                 </p>
                 <button>Go</button>
             </motion.div>
             <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
                 <h2>Team Leader</h2>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    At voluptas asperiores illo hic expedita repellat est saepe dolorem voluptates culpa alias,
-                    debitis amet tempora vel et iusto. Nobis, necessitatibus cupiditate!
+                <p> As a natural leader, I take pride in guiding teams toward success. I am the leader of our entire Software 
+                    Engineering batch, where I coordinate and inspire my peers to excel in both academics and projects. Additionally 
+                    I started and lead the Kylix Development Team.
                 </p>
                 <button>Go</button>
             </motion.div>
