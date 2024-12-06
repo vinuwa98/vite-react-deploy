184dbc9165f1ea6a3d70851789d2045e10e87836
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2024-12-06 22:41:20 +0530

    Add LinkedIn Link

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
           <a href="https://www.instagram.com/vinuwa.social/"><img src="/instagram.png" alt="Instagram" /></a>
           <a href="https://www.youtube.com/@VinodWarnakulasooriya975/"><img src="/youtube.png" alt="YouTube" /></a>
           <a href="https://github.com/vinuwa98"><img src="/github.png" alt="github" /></a>
+          <a href="https://www.linkedin.com/in/vinuwa"><img src="/linkedin.jpg" alt="github" /></a>
         </div>
       </div>
     </div>
